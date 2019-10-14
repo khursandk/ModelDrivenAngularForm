b146c7f19d13566533e11f7b4921c3518962f86d
khursandk <56547198+khursandk@users.noreply.github.com>
2019-10-14 20:47:56 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Model Driven Angular Form 
+  title = 'Model Driven Angular Form'; 
 }
